6a028d9e748e171ea0e6c53d77701da9490a16f0
GlennOcean10 <82073007+GlennOcean10@users.noreply.github.com>
2021-04-09 14:28:53 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -18,7 +18,7 @@ var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
   L.geoJSON(data, {
-    // Color all alert polygons orange, but color Severe polygons red
+    // Color all alert polygons orange, but color Severe polygons #2DF936
     style: function(feature){
       var alertColor = 'orange';
       if (feature.properties.severity === 'Severe') alertColor = '#2DF936';
